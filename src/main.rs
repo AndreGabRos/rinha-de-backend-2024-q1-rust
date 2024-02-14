50f89c84ee4122294b1e0c4597dfe09d7f5eeff7
Gustavo Mantovani <mantovanig000@gmail.com>
2024-02-14 17:31:56 -0300

    Aprendendo actix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -14,15 +19,31 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+
+            //Informações do app
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+
             .service(hello)
             .service(echo)
+            .service(web::scope("/app")
+                // ...so this handles requests for `GET /app/index.html`
+                .route("/index.html", web::get().to(index)),)
             .route("/hey", web::get().to(manual_hello))
+
     })
-    .bind(("0.0.0.0", 8000))?
+    .bind(("127.0.0.1", 8000))?
     .run()
     .await
 }
